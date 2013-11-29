b62d000e67221611dd2c90f3c105da84669cb6d8
yannis <yannis@ianos>
2013-11-29 09:14:36 +0200

    Work in progress

--- src/main/java/fraglabs/jaxbworkshop/model/PersonAdapter.java
+++ src/main/java/fraglabs/jaxbworkshop/model/PersonAdapter.java
@@ -4,15 +4,6 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 public class PersonAdapter extends XmlAdapter<AdaptedPerson, Person> {
 
-    private String injected;
-
-
-    public PersonAdapter() {  }
-
-    public PersonAdapter(String injected) {
-        this.injected = injected;
-    }
-
     @Override
     public Person unmarshal(AdaptedPerson v) throws Exception {
         return new Person(v.getFirstName() + " " + v.getLastName());
@@ -21,6 +12,6 @@ public class PersonAdapter extends XmlAdapter<AdaptedPerson, Person> {
     @Override
     public AdaptedPerson marshal(Person v) throws Exception {
         String[] name = v.getName().split("\\s");
-        return new AdaptedPerson(name[0], name[1] + injected);
+        return new AdaptedPerson(name[0], name[1]);
     }
 }
--- src/test/java/fraglabs/jaxbworkshop/MarshalTests.java
+++ src/test/java/fraglabs/jaxbworkshop/MarshalTests.java
@@ -1,14 +1,28 @@
 package fraglabs.jaxbworkshop;
 
+import com.sun.xml.internal.ws.util.xml.ContentHandlerToXMLStreamWriter;
 import fraglabs.jaxbworkshop.model.Company;
 import fraglabs.jaxbworkshop.model.Person;
 import fraglabs.jaxbworkshop.model.PersonAdapter;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.helpers.DefaultValidationEventHandler;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,13 +47,37 @@ public class MarshalTests {
     }
 
     @Test
-    public void override() throws JAXBException {
+    public void overrideContentHandler() throws JAXBException, IOException, XMLStreamException {
         JAXBContext context = JAXBContext.newInstance(Company.class);
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        marshaller.setAdapter(new PersonAdapter(" *SET* "));
-        marshaller.marshal(company, System.out);
+
+        OutputStream outputStream = System.out;
+        final XMLStreamWriter out = XMLOutputFactory.newInstance().createXMLStreamWriter(
+                new OutputStreamWriter(outputStream, "utf-8"));
+        ContentHandlerToXMLStreamWriter handler = new ContentHandlerToXMLStreamWriter(out);
+        marshaller.marshal(company, handler);
+
+        out.close();
+
     }
+
+    @Test
+    public void printXml() throws JAXBException, UnsupportedEncodingException, XMLStreamException {
+        XMLStreamWriter out = XMLOutputFactory.newInstance().createXMLStreamWriter(
+                new OutputStreamWriter(System.out, "utf-8"));
+
+        out.writeStartDocument();
+        out.writeStartElement("doc");
+
+        out.writeStartElement("title");
+        out.writeCharacters("Document Title");
+        out.writeEndElement();
+
+        out.writeEndElement();
+        out.writeEndDocument();
+
+        out.close();    }
 
 
 }
